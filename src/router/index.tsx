b70af8dd6b1aed9b416fab1f1a78794bc3c9cfdb
Filipe Bessa <filipebessa.web@gmail.com>
2024-09-02 11:28:38 -0300

    Update index.tsx

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,16 +1,16 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import DashboardPage from '~/pages/Dashboard';
 import NewUserPage from '~/pages/NewUser';
 import routes from './routes';
 
 const AppRouter = () => {
   return (
-    <HashRouter basename="/">
+    <BrowserRouter basename="/">
       <Routes>
         <Route index path={routes.dashboard} element={<DashboardPage />} />
         <Route path={routes.newUser} element={<NewUserPage />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
