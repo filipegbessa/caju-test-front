c7940a54b1d09349ad3e83fe860c31b92e8500ec
Filipe Bessa <filipebessa.web@gmail.com>
2024-09-01 23:28:18 -0300

    Update ci.yml

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,14 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import DashboardPage from '~/pages/Dashboard';
 import NewUserPage from '~/pages/NewUser';
+import routes from './routes';
 
 const AppRouter = () => {
   return (
     <HashRouter basename="/">
       <Routes>
-        <Route path="/" element={<DashboardPage />} />
-        <Route path="/new-user" element={<NewUserPage />} />
+        <Route index path={routes.dashboard} element={<DashboardPage />} />
+        <Route path={routes.newUser} element={<NewUserPage />} />
       </Routes>
     </HashRouter>
   );
