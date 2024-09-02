abcd312bd34944457d61285caec137ea096a14fd
Filipe Bessa <filipebessa.web@gmail.com>
2024-09-02 11:17:18 -0300

    Update registration.ts

--- src/utils/registration.ts
+++ src/utils/registration.ts
@@ -2,7 +2,7 @@ import { RegistrationStatusEnum } from '~/enum';
 import { IRegistration } from '~/types';
 
 export const filterRegistrationsByStatus = (
-  registrations: IRegistration[] = [],
+  registrations: IRegistration[],
   status: RegistrationStatusEnum
 ): IRegistration[] =>
-  registrations.filter((registration) => registration.status === status);
+  registrations?.filter((registration) => registration.status === status);
